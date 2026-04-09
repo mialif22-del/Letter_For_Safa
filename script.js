7b15732dd3294de45d845aba71524a3efce204ad
Aleeeepppp <mialif22@gmail.com>
2026-04-09 17:40:30 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,79 +1,72 @@
-//Elements
+// Elements
 const envelope = document.getElementById("envelope-container");
 const letter = document.getElementById("letter-container");
-const noBtn =document.querySelector("no-btn");
-const yesBtn =document.querySelector(".btn[alt='Yes']");
+const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".btn[alt='Yes']");
 
-const title =document.getElementById("letter-title");
-const catImg =document.getElementById("letter-cat");
-const buttons =document.getElementById("letter-buttons");
-const finalText =document.getElementById("final-text");
+const title = document.getElementById("letter-title");
+const catImg = document.getElementById("letter-cat");
+const buttons = document.getElementById("letter-buttons");
+const finalText = document.getElementById("final-text");
 
-//Click Envelope
+// Click Envelope
 
-envelope.addEventListener("click", () =>  {
+envelope.addEventListener("click", () => {
     envelope.style.display = "none";
     letter.style.display = "flex";
 
-    setTimeout( () =>
-        {
-            document.querySelector(".letter-window").classList.add("open");
+    setTimeout( () => {
+        document.querySelector(".letter-window").classList.add("open");
+    },50);
+});
 
+// Logic to move the NO btn
 
+noBtn.addEventListener("mouseover", () => {
+    const min = 200;
+    const max = 200;
 
-        }, 50);
+    const distance = Math.random() * (max - min) + min;
+    const angle = Math.random() * Math.PI * 2;
 
-    });
+    const moveX = Math.cos(angle) * distance;
+    const moveY = Math.sin(angle) * distance;
 
-    // Logic to Move the NO btn
+    noBtn.style.transition = "transform 0.3s ease";
+    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+});
 
-    noBtn.addEventListener("mouseover", () => {
-        const min = 200;
-        const max = 200;
+// Logic to make YES btn to grow
 
-        const distance = Math.random() * (max-min) + min;
-        const angle = Math.random() * Math.PI * 2;
+// let yesScale = 1;
 
-        const moveX = Math.cos(angle) * distance;
-        const moveY = Math.sin(angle) * distance;
+// yesBtn.style.position = "relative"
+// yesBtn.style.transformOrigin = "center center";
+// yesBtn.style.transition = "transform 0.3s ease";
 
-        noBtn.style.transition = "transform 0.3s ease";
-        noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+// noBtn.addEventListener("click", () => {
+//     yesScale += 2;
 
-    })
+//     if (yesBtn.style.position !== "fixed") {
+//         yesBtn.style.position = "fixed";
+//         yesBtn.style.top = "50%";
+//         yesBtn.style.left = "50%";
+//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+//     }else{
+//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+//     }
+// });
 
-    // Logic to make YES btn to grow
+// YES is clicked
 
-   /* let yesScale = 1;
+yesBtn.addEventListener("click", () => {
+    title.textContent = "Yippeeee!";
 
-    yesBtn.style.position ="relative"
-    yesBtn.style.transformOrigin = "center center";
-    yesBtn.style.transition = "transform 0.3s ease";
+    catImg.src = "cat_dance.gif";
 
-    noBtn.addEventListener("click", () => {
-        yesScale += 2;
+    document.querySelector(".letter-window").classList.add("final");
 
-        if (yesBtn.style.position !== "fixed"){
-            yesBtn.style.position = "fixed";
-            yesBtn.style.top = "50%";
-            yesBtn.style.left = "50%";
-            yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+    buttons.style.display = "none";
 
-        }else{
-            yesBtn.style.transform =  `translate(-50%, -50%) scale(${yesScale})`;
-        }
-    });
-
-    // YES is Clicked */
-
-    yesBtn.addEventListener("click", () => {
-        title.textContent = "Yippeeee!";
-
-        catImg.src ="cat_dance.gif";
-
-        document.querySelector(".letter-window").classList.add("final");
-
-        buttons.style.display = "none";
-
-        finalText.style.display = "block";
-    });
+    finalText.style.display = "block";
+});
